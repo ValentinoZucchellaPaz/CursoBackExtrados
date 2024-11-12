2f5e3b02042260ce064bd08ced55f2f0543189d8
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2024-11-11 21:07:06 -0300

    tarea1-8reinas-1solucion

---
+++ POO/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tarea1_8Reinas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tablero tablero = new Tablero();
+
+        if (tablero.Resolver())
+        {
+            Console.WriteLine("Solución encontrada:");
+            tablero.MostrarTablero();
+        }
+        else
+        {
+            Console.WriteLine("No se encontró una solución.");
+        }
+    }
+}
